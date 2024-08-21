a9e6b3fbdb0c9f32bacba6f0bcab76d40ec48375
DESKTOP-D4GTUC2\soo50 <soo5087@naver.com>
2024-08-21 20:22:35 +0900

    [setup] code push
    - 기본 씬 구성
    - 기본 Cam Controller 구현
    - 기본 Camera View 구현

---
+++ Assets/Scripts/3.InGame/ObejctControlCom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class ObejctControlCom : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
